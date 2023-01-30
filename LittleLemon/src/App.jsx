fc4a69072fd2cd0a5fed0185b460e5b701cfdfb0
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-01-30 17:09:22 +0300

    The Loading Func

--- LittleLemon/src/App.jsx
+++ LittleLemon/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading/Loading";
+import "./App.css";
 
 function App() {
+  //! The Loader
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  });
 
-  return (
-    <h1>
-      Hiff
-    </h1>
-  )
+  return <>{isLoading ? <Loading /> : <h1>Hi</h1>}</>;
 }
 
-export default App
+export default App;
