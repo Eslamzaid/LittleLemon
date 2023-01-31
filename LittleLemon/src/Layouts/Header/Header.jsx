543be9e05cb1d49377cf84205cd18c1652cfe9f9
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-01-31 17:34:15 +0300

    The hamburger menu

--- LittleLemon/src/Layouts/Header/Header.jsx
+++ LittleLemon/src/Layouts/Header/Header.jsx
@@ -3,37 +3,87 @@ import theLogo from "./../../assets/Logo-removebg-preview.png";
 import Menu from "../../RComponenets/Menu/Menu";
 import About from "../../RComponenets/About/About";
 import { Routes, Route, Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
     <>
       <nav>
-        <ul class="container-fluid">
+        <ul className="desktop-view">
           <li>
             <Link to="/">
-              <img src={theLogo} alt="Our restaurant logo" />
+              <img width="200rem" src={theLogo} alt="Our restaurant logo" />
             </Link>
           </li>
           <li>
-            <Link to="/">
+            <Link id="TheLinks" to="/">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/AboutUs">About</Link>
+            <Link id="TheLinks" to="/AboutUs">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/TheMenu">Menu</Link>
+            <Link id="TheLinks" to="/TheMenu">
+              Menu
+            </Link>
           </li>
           <li>
-            <Link to="/Reservations">Reservations</Link>
+            <Link id="TheLinks" to="/Reservations">
+              Reservations
+            </Link>
           </li>
           <li>
-            <Link to="/Order">Order</Link>
+            <Link id="TheLinks" to="/Order">
+              Order
+            </Link>
           </li>
           <li>
-            <Link to="/Login">Login</Link>
+            <Link id="TheLinks" to="/Login">
+              Login
+            </Link>
           </li>
+        </ul>
+        <ul className="phone-view">
+          <li>
+            <Link to="/">
+              <img width="200rem" src={theLogo} alt="Our restaurant logo" />
+            </Link>
+          </li>
+          <div className="TheMiniFather">
+            <li>
+              <Link id="TheLinksPh" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link id="TheLinksPh" to="/AboutUs">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link id="TheLinksPh" to="/TheMenu">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link id="TheLinksPh" to="/Reservations">
+                Reservations
+              </Link>
+            </li>
+            <li>
+              <Link id="TheLinksPh" to="/Order">
+                Order
+              </Link>
+            </li>
+            <li>
+              <Link id="TheLinksPh" to="/Login">
+                Login
+              </Link>
+            </li>
+          </div>
         </ul>
       </nav>
       <Routes>
