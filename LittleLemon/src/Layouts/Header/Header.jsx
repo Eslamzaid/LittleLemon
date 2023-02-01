5338851510420c1e41b1248307ae5dddf6243c06
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-01 11:31:13 +0300

    Working

--- LittleLemon/src/Layouts/Header/Header.jsx
+++ LittleLemon/src/Layouts/Header/Header.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import theLogo from "./../../assets/Logo-removebg-preview.png";
 import Menu from "../../RComponenets/Menu/Menu";
 import About from "../../RComponenets/About/About";
+import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 
 const Header = () => {
+  const [responsive, setResponsive] = useState(true);
+
+  const handleOnClick = () => {
+    setResponsive(!responsive);
+  };
+
   return (
     <>
       <nav>
@@ -48,46 +55,63 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-          <FontAwesomeIcon icon={faBars} />
-        <ul className="phone-view">
-          <li id="ThePhoto">
-            <Link to="/">
-              <img width="200rem" src={theLogo} alt="Our restaurant logo" />
-            </Link>
-          </li>
-          <div className="TheMiniFather">
-            <li>
-              <Link id="TheLinksPh" className="MakeIt" to="/">
-                Home
+        <div className="MakeItLeft"></div>
+        {responsive ? (
+          <FontAwesomeIcon
+            className="MakeItLeft"
+            icon={faXmark}
+            onClick={handleOnClick}
+          />
+        ) : (
+          <FontAwesomeIcon
+            className="MakeItLeft"
+            icon={faBars}
+            onClick={handleOnClick}
+          />
+        )}
+        {responsive ? (
+          <ul className="phone-view">
+            <li id="ThePhoto">
+              <Link to="/">
+                <img width="200rem" src={theLogo} alt="Our restaurant logo" />
               </Link>
             </li>
-            <li>
-              <Link id="TheLinksPh" to="/AboutUs">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link id="TheLinksPh" to="/TheMenu">
-                Menu
-              </Link>
-            </li>
-            <li>
-              <Link id="TheLinksPh" to="/Reservations">
-                Reservations
-              </Link>
-            </li>
-            <li>
-              <Link id="TheLinksPh" to="/Order">
-                Order
-              </Link>
-            </li>
-            <li>
-              <Link id="TheLinksPh" to="/Login">
-                Login
-              </Link>
-            </li>
-          </div>
-        </ul>
+            <div className="TheMiniFather">
+              <li>
+                <Link id="TheLinksPh" className="MakeIt" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link id="TheLinksPh" to="/AboutUs">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link id="TheLinksPh" to="/TheMenu">
+                  Menu
+                </Link>
+              </li>
+              <li>
+                <Link id="TheLinksPh" to="/Reservations">
+                  Reservations
+                </Link>
+              </li>
+              <li>
+                <Link id="TheLinksPh" to="/Order">
+                  Order
+                </Link>
+              </li>
+              <li>
+                <Link id="TheLinksPh" to="/Login">
+                  Login
+                </Link>
+              </li>
+            </div>
+          </ul>
+        ) : (
+          ""
+        )}
       </nav>
       <Routes>
         <Route path="/AboutUs" element={<About />} />
