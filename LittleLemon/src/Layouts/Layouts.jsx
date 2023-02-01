64b5a51ef03c981dc0ee656132eb04732e64303d
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-01 16:24:46 +0300

    The nav

--- LittleLemon/src/Layouts/Layouts.jsx
+++ LittleLemon/src/Layouts/Layouts.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import About from "../RComponenets/About/About";
 import Header from "./Header/Header";
 import "./Layouts.css";
 
 function Layouts() {
   return (
     <>
-      <Header />
+      <Header />  
     </>
   );
 }
