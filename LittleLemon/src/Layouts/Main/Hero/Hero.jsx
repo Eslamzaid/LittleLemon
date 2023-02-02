825dd5a131cc0c53aa64469dff2c93b9d5bc0424
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-02 14:35:20 +0300

    The hero

--- LittleLemon/src/Layouts/Main/Hero/Hero.jsx
+++ LittleLemon/src/Layouts/Main/Hero/Hero.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import TheBurger from "./../../../assets/Main/TheBurger.png";
+import "./Hero.css";
 
 function Hero() {
-    return (
-        <>
-         <h1>My name is ahemd</h1>   
-        </>
-    )
+  return (
+    <article className="TheHero">
+      <section className="TheFirstBoy">
+        <div className="TheFirstGrand">
+          <h2>
+            We are a family owned<br/> Mediterranean restaurant,<br/> focused on
+            traditional<br/> recipes served with a modern twist.
+          </h2>
+          <button>Order Now</button>
+        </div>
+        <div className="TheSecondGrand">
+          <img id="TheBurgerImg" src={TheBurger} alt="Delicious Burger" />
+        </div>
+      </section>
+    </article>
+  );
 }
 
-export default Hero
+export default Hero;
