ccf2967d26b3414aab3c3bc70c69c5d6f6f91ee2
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-05 16:38:59 +0300

    The search

--- LittleLemon/src/Layouts/Main/Main.jsx
+++ LittleLemon/src/Layouts/Main/Main.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import './Main.css'
-import SearchF from './SearchF/SearchF'
+import React from "react";
+import Hero from "./Hero/Hero";
+import SearchF from "./SearchF/SearchF";
+import { CartProvider } from "react-use-cart";
+import Cart from "./Cart/Cart";
+import "./Main.css";
 
 function Main() {
-    return (
-        <main>
-            <Hero/>
-            <SearchF/>
-        </main>
-    )
+  return (
+    <main>
+      <Hero />
+      <CartProvider>
+        <SearchF />
+        <Cart />
+      </CartProvider>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
--- LittleLemon/src/Layouts/Main/SearchF/SearchF.jsx
+++ LittleLemon/src/Layouts/Main/SearchF/SearchF.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+import Teset from './../../../assets/Icons/Arrow 2.png'
 import MenuD from "./SearchD";
 import "./SearchF.css";
+
+const reducer = (state, action) => {
+  if(action.type === "addOne") return {num: state.num + 1}
+  return state
+}
 
 function SearchF() {
   const [quiry, setQuiry] = useState("");
   const [item, setItem] = useState(MenuD);
+  const [another, setAnother] = useState(false)
+  const initialState = {num: 0}
+  const [state, dispatch] = useReducer(reducer,initialState)
+  const [num, setNum] = useState(0)
+
+
   const filterMenu = (category) => {
     const updateItems = MenuD.filter((curElem) => {
       return curElem.category === category;
@@ -12,6 +24,7 @@ function SearchF() {
 
     setItem(updateItems);
   };
+
   return (
     <article className="TheFather">
       <section className="FSection">
@@ -35,14 +48,19 @@ function SearchF() {
       </section>
       <section>
         {item
-          .filter((itemm) => itemm.name.toLowerCase().includes(quiry))
-          .map((ele) => {
-            const { id, image, name, price, alt } = ele;
+          .filter((itemm) => itemm.name.toLowerCase().includes(quiry)) 
+         .map((ele) => {
+            const { id, image, name, price, alt, fav } = ele;
             return (
               <div key={id}>
-                <h3>{name}</h3>
-                <img src={image} width="200px" alt={alt} />
-                <p>Price: {price}</p>
+    
+                <div>
+                  <img src={image} width="200px" alt={alt} />
+                </div>
+                <div>
+                  <h3>{name}</h3>
+                  <p>Price: {price}</p>
+                </div>
               </div>
             );
           })}
