ad75765a24a19937449af93bcde1a9a76f54c200
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-10 15:02:48 +0300

    The Main

--- LittleLemon/src/Layouts/Main/Main.jsx
+++ LittleLemon/src/Layouts/Main/Main.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from "react";
 import Hero from "./Hero/Hero";
 import SearchF from "./SearchF/SearchF";
-import Cart from "./Cart/Cart";
-import { CartProvider ,useCart} from "react-use-cart";
 import "./Main.css";
 
 function Main() {
