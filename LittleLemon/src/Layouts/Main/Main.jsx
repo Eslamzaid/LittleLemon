0f86446cbc7ab49a8870f54302db3c5c3d722c5d
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-11 18:47:11 +0300

    Commit

--- LittleLemon/src/Layouts/Main/Main.jsx
+++ LittleLemon/src/Layouts/Main/Main.jsx
@@ -2,16 +2,18 @@ import React, { useRef } from "react";
 import Hero from "./Hero/Hero";
 import SearchF from "./SearchF/SearchF";
 import "./Main.css";
+import OrderTable from "./TheTable/Table";
 
 function Main() {
-  const OrderNow = useRef(null)
+  const OrderNow = useRef(null);
   const focusInput = () => {
-    OrderNow.current.focus()
-  }
+    OrderNow.current.focus();
+  };
   return (
     <main>
-      <Hero connectMenu={focusInput}/>
-        <SearchF connect={OrderNow}/>
+      <Hero connectMenu={focusInput} />
+      <SearchF connect={OrderNow} />
+      <OrderTable/>
     </main>
   );
 }
---
+++ LittleLemon/src/Layouts/Main/TheTable/Table.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { submitAPI, fetchAPI } from "./Sources";
+import { useState } from "react";
+import "./Table.css";
+
+function OrderTable() {
+  const [table, setTable] = useState();
+
+  return (
+    <div>
+      <h1>TheTable</h1>
+      <div>
+        <form>
+          <label>Choose date</label>
+          <input type="date" id="res-date" />
+          <label>Choose time</label>
+          <select id="res-time ">
+            <option>17:00</option>
+            <option>18:00</option>
+            <option>19:00</option>
+            <option>20:00</option>
+            <option>21:00</option>
+            <option>22:00</option>
+          </select>
+          <label>Number of guests</label>
+          <input type="number" placeholder="1" min="1" max="10" id="guests" />
+          <label>Occasion</label>
+          <select id="occasion">
+            <option>Birthday</option>
+            <option>Anniversary</option>
+          </select>
+          <button type="submit">Make Your reservation</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default OrderTable;
