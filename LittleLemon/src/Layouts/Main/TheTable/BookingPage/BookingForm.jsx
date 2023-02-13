d870d6622454cf25ebd0be7e9dc7e5003b32b1c4
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-13 11:56:29 +0300

    Commit

--- LittleLemon/src/Layouts/Main/TheTable/BookingPage/BookingForm.jsx
+++ LittleLemon/src/Layouts/Main/TheTable/BookingPage/BookingForm.jsx
@@ -1,74 +1,50 @@
-import React, { useEffect } from "react";
-import { submitAPI, fetchAPI } from "../Sources";
-import Time from "../Sources";
-import { useState } from "react";
+import React, { useState } from "react";
+import MenuD from "./../Sources";
+import DatePicker from "react-date-picker";
 import "./Table.css";
 
-
-
 function BookingForm() {
-  const [date, setDate] = useState()
-  const [time, setTime] = useState()
-  const [guestsNum, setGuestsNum] = useState()
-  const [occasion, setOccasion] = useState()
-  useEffect(
-    () => {
-      console.log(date);
-      console.log(time);
-      console.log(guestsNum);
-      console.log(occasion);
-    },
-    [date,time,guestsNum,occasion]
-  );
-
+  const [time, setTime] = useState(MenuD);
+  const [value, onChange] = useState(new Date());
+  const date = new Date();
+  const daHours = date.getHours();
+  const daMin = date.getMinutes();
+  const daSec = date.getSeconds();
+  const deDay = date.getDay();
+  const deMon = date.getMonth();
+  const deYear = date.getFullYear();
   return (
     <section className="fathofAll">
-      <h2>Book a table</h2>
-      <form>
-        <label htmlFor="res-date">Choose date</label>
-        <input
-          required
-          type="date"
-          id="res-date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        />
-        {date == "01/01/2023" ? <h1>yes</h1> : <h1>no</h1>}
-        {time == 3 ? <h1>😁</h1>: <h1>👎</h1>}
-        <label htmlFor="res-pmnoram">Am/Pm</label>
-        <select required id="res-pmnoram">
-          <option>Am/Pm</option>
-          <option>Am</option>
-          <option>Pm</option>
-        </select>
-        <label htmlFor="res-time">Choose time</label>
-        <select required id="res-time " onChange={(e) => setTime(e.target.value)}>
-          <option>Select Time</option>
-          <option>3</option>
-          <option aria-selected="false">4</option>
-          <option>5</option>
-          <option>7</option>
-          <option>8</option>
-          <option>9</option>
-        </select>
-        <label htmlFor="guests">Number of guests</label>
-        <input
-          required
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          id="guests"
-          onChange={(e) => setGuestsNum(e.target.value)}
-        />
-        <label htmlFor="occasion">Occasion</label>
-        <select required id="occasion" onChange={(e) => setOccasion(e.target.value)}>
-          <option>Select an occasion</option>
-          <option>Birthday</option>
-          <option>Anniversary</option>
-        </select>
-        <button type="submit">Make Your reservation</button>
-      </form>
+      <h1>Book a table</h1>
+      <DatePicker
+        required
+        calendarType="Arabic"
+        defaultView="month"
+        onChange={onChange}
+        value={value}
+      />
+      {/* {value == 'Mon Feb 13 2023 11:37:28 GMT+0300 (Arabian Standard Time)' ? <h1>Yes</h1> : <h1>No</h1>} */}
+      {value == `` ? <h1>Yes</h1> : <h1>No</h1>}
+      {console.log(value)}
+      <div className="calender">
+        <form>
+          <label htmlFor="Thecal">Pick a date</label>
+          <input id="Thecal" type="date" />
+        </form>
+      </div>
+      <div>
+        {time.map((ele, ind) => {
+          const { time, when } = ele;
+          // console.log(time);
+          return (
+            <div key={ind}>
+              <h1>
+                {time} {when}
+              </h1>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
--- LittleLemon/src/Layouts/Main/TheTable/Sources.js
+++ LittleLemon/src/Layouts/Main/TheTable/Sources.js
@@ -1,52 +1,52 @@
-const Time = [
+const MenuD = [
     {
         time: 1,
-        when: am,
+        when: 'am',
     },
     {
         time: 2,
-        when: am,
+        when: 'am',
     },
     {
         time: 3,
-        when: am,
+        when: 'am',
     },
     {
         time: 4,
-        when: am,
+        when: 'am',
     },
     {
         time: 5,
-        when: am,
+        when: 'am',
     },
     {
         time: 6,
-        when: am,
+        when: 'am',
     },
     {
         time: 2,
-        when: pm,
+        when: 'pm',
     },
     {
         time: 3,
-        when: pm,
+        when: 'pm',
     },
     {
         time: 5,
-        when: pm,
+        when: 'pm',
     },
     {
         time: 6,
-        when: pm,
+        when: 'pm',
     },
     {
         time: 7,
-        when: pm,
+        when: 'pm',
     },
     {
         time: 8,
-        when: pm,
+        when: 'pm',
     },
 ]
 
-export default Time
+export default MenuD
