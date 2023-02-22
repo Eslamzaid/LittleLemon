4a9b5b68cba20aebdfbec233092b51a633f57da4
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-22 12:16:00 +0300

    Commit

--- LittleLemon/src/Layouts/Main/TheSlid/Slider.jsx
+++ LittleLemon/src/Layouts/Main/TheSlid/Slider.jsx
@@ -5,6 +5,10 @@ import PauseOnHover from "./TheContent";
 import "./Slider.css";
 
 const Slider = () => {
+  var letsGoo = 4
+  if (window.screen.availWidth <= 830){
+    letsGoo = 1
+  }
   return (
     <section className="TheFathher">
       <div className="FEslamSection">
--- LittleLemon/src/Layouts/Main/TheSlid/TheContent.jsx
+++ LittleLemon/src/Layouts/Main/TheSlid/TheContent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Numo from "./../../../assets/Main/noma.png";
 import AA from "./../../../assets/Main/aa.png";
 import Tglobe from "./../../../assets/Main/theglob.png";
@@ -9,54 +9,97 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default class PauseOnHover extends Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      pauseOnHover: false,
-      adaptiveHeight: true,
-      arrows: false,
-    };
-    return (
-      <div className="TheBIgdog">
-        <Slider {...settings}>
-          <div className="TheImgges">
-            <img src={Numo} alt="numo is one of our partenrs" id="SoemWIthg" />
-          </div>
-          <div className="TheImgges">
-            <img src={AA} alt="A.a is one of our partenrs" id="SoemWIthg" />
-          </div>
-          <div className="TheImgges">
-            <img
-              src={Tglobe}
-              alt="The globe is one of our partenrs"
-              id="SoemWIthg"
-            />
-          </div>
-          <div className="TheImgges">
-            <img
-              src={BurgerK}
-              alt="Burger King is one of our partenrs"
-              id="SoemWIthg"
-            />
-          </div>
-          <div className="TheImgges">
-            <img
-              src={Subway}
-              alt="Subway is one of our partenrs"
-              id="SoemWIthg"
-            />
-          </div>
-          <div className="TheImgges">
-            <img src={KFC} alt="Kfc is one of our partenrs" id="SoemWIthg" />
-          </div>
-        </Slider>
-      </div>
-    );
-  }
+export default function PauseOnHover() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    adaptiveHeight: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1044,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 834,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="TheBIgdog">
+      <Slider {...settings}>
+        <div className="TheImgges">
+          <img src={Numo} alt="numo is one of our partenrs" id="SoemWIthg" />
+        </div>
+        <div className="TheImgges">
+          <img src={AA} alt="A.a is one of our partenrs" id="SoemWIthg" />
+        </div>
+        <div className="TheImgges">
+          <img
+            src={Tglobe}
+            alt="The globe is one of our partenrs"
+            id="SoemWIthg"
+          />
+        </div>
+        <div className="TheImgges">
+          <img
+            src={BurgerK}
+            alt="Burger King is one of our partenrs"
+            id="SoemWIthg"
+          />
+        </div>
+        <div className="TheImgges">
+          <img
+            src={Subway}
+            alt="Subway is one of our partenrs"
+            id="SoemWIthg"
+          />
+        </div>
+        <div className="TheImgges">
+          <img src={KFC} alt="Kfc is one of our partenrs" id="SoemWIthg" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
